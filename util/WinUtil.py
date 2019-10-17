b2e7e2cab75ce5c746500304909adc1ea34cab9e
xili <xili2016@foxmail.com>
2019-10-17 15:34:15 +0800

    参数错误处理

--- util/WinUtil.py
+++ util/WinUtil.py
@@ -63,11 +63,11 @@ def log_runnable(process: subprocess.Popen, task_logger: logging.Logger):
 
 # func: 任务进行中的执行函数
 # task_terminate_func: 任务终止时的执行函数
-def __runnable(process: subprocess.Popen, name, func, task_terminate_func, *args, **kwargs):
+def __runnable(process: subprocess.Popen, name, func, task_terminate_func, logger):
     ThreadHandler.get_thread_handler().put_thread(name, threading.current_thread())
     logger.info("启动线程{} - {}".format(name, threading.current_thread().getName()))
     while process.poll() is None:
-        func(process, *args, **kwargs)
+        func(process, logger)
     logger.info("线程{}结束运行 {}".format(name, threading.current_thread().getName()))
     ThreadHandler.get_thread_handler().pop_thread(name)
     if task_terminate_func and hasattr(task_terminate_func, '__call__'):
@@ -75,11 +75,11 @@ def __runnable(process: subprocess.Popen, name, func, task_terminate_func, *args
 
 
 # 启动一个后台任务
-def run_task(name, command, func=log_runnable, task_terminate_func=None, shell=False, *args, **kwargs):
+def run_task(name, command, func=log_runnable, task_terminate_func=None, shell=False, logger=logger):
     logger.warning('执行系统命令: {}'.format(command))
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=shell)
     ProcessHandler.get_process_handler().put_process(name, p)  # 将进程统一管理
-    thread = threading.Thread(target=__runnable, args=(p, name, func, task_terminate_func, *args, *kwargs))
+    thread = threading.Thread(target=__runnable, args=(p, name, func, task_terminate_func, logger))
     thread.setDaemon(True)  # 设置为守护线程, 在应用退出时不会阻塞
     thread.start()
     return p
